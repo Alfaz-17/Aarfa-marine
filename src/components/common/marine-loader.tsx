cdb6489a295a66258a1879f8837052cbce3dccc8
Alfaz Bilakhiya <alfazbilakhiya17@gmail.com>
2026-06-28 12:44:34 +0530

    .

--- src/components/common/marine-loader.tsx
+++ src/components/common/marine-loader.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export const MarineLoader = () => {
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-secondary">
+    <div className="w-full h-screen flex justify-center items-center bg-slate-50">
       <div className="flex flex-col items-center">
-        <div className="w-16 h-16 border-4 border-secondary-light/20 border-t-accent rounded-full animate-spin"></div>
-        <p className="mt-4 text-xs font-mono font-bold uppercase tracking-widest text-secondary-light">Syncing systems...</p>
+        <div className="w-16 h-16 border-4 border-primary-light/20 border-t-primary rounded-full animate-spin"></div>
+        <p className="mt-4 text-xs font-mono font-bold uppercase tracking-widest text-primary-light">Syncing systems...</p>
       </div>
     </div>
   );
